95a4ed3ee38df6e9216855725f08d5e5b3f0bea4
ava-mirtl <ava_mirtl@mail.ru>
2024-02-02 16:03:59 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  data(){
+    return{
+      city: ""
+    }
+  }
+}</script>
+
+<template>
+<div className="wrapper">
+  <h1>Weather App </h1>
+  <p>Узнать погоду в {{ city }}</p>
+  <input type="text" v-model="city" placeholder="Введите город">
+  <button>Получить погоду</button>
+</div>
+</template>
+
+<style scoped>
+.wrapper{
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background-color: black;
+  color: white;
+}
+.wrapper h1{
+margin-top: 50px;
+}
+.wrapper p{
+  margin-top: 20px;
+}
+.wrapper input{
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+}
+.wrapper input:focus{
+border-bottom-color: #f8d8ce;
+}
+.wrapper button{
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+.wrapper button:hover{
+  transform: scale(1.1) translateY(-5px);
+}
+</style>
